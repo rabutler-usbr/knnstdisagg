cadd4ebccd389a4c67bd2c68b594b9eabddbd42a
Alan Butler <rabutler@usbr.gov>
2018-11-29 17:37:51 -0700

    updating the package help page

--- R/knnstdisagg.R
+++ R/knnstdisagg.R
@@ -1,6 +1,12 @@
-
-#' @keywords internal
-"_PACKAGE"
-
+#' @section Acknowledgements:
+#' This package implements the methods developed by Ken Nowak, Jim Prairie et
+#' al.
+#'
+#' @section Reference:
+#' Nowak, K., J. Prairie, B. Rajagopalan, and U. Lall (2010), A nonparametric
+#' stochastic approach for multisite disaggregation of annual to daily
+#' streamflow, Water Resour. Res., 46, W08529,
+#' [doi:10.1029/2009WR008530.](https://agupubs.onlinelibrary.wiley.com/doi/full/10.1029/2009WR008530)
+#'
 #' @importFrom assertthat assert_that
-NULL
+"_PACKAGE"
