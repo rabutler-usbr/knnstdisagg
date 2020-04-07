70f77bc1ed9377919647794634a33a9bb8a23cbe
Alan Butler <rabutler@usbr.gov>
2020-04-07 16:47:57 -0600

    added Meko et al. 2007 data to use as example

---
+++ R/data.R
@@ -0,0 +1,19 @@
+#' Upper Colorado River Flow Reconstruction.
+#'
+#' A dataset containing paleo reconstructed hydrology for the Colorado River at
+#' Lees Ferry for 762 - 2005.
+#'
+#' @format A matrix with 1244 rows and 2 columns:
+#'   - column 1 = water_year: the water year (October - September)
+#'   - column 2 = lees_ferry: reconstructed flow at Lees Ferry in acre-feet
+#'
+#' @references Meko, D.M., Woodhouse, C.A., Baisan, C.A., Knight, T., Lukas,
+#' J.J., Hughes, M.K., and Salzer, M.W. 2007. Medieval Drought in the Upper
+#' Colorado River Basin. Geophysical Research Letters 34, L10705.
+#'
+#' @source Description of data:
+#'   \url{https://www.treeflow.info/content/colorado-r-lees-ferry-az-meko}
+#'
+#' @source Data download:
+#'   \url{http://www.riversimulator.org/Resources/ClimateData/Meko762-2005.xls}
+"meko"
